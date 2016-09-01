e91e23d960b7adc69b6e72fa4118f0a006c42fee
BuildTools <unconfigured@null.spigotmc.org>
2016-09-01 09:22:44 +0200

    Projectiles
    Added projectile system and tooltips

---
+++ MightyWizards/Assets/ScriptableObjects/Projectile/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(menuName = "Projectiles/Projectile")]
+public class Projectile : ScriptableObject {
+
+    [Tooltip("The prefab with the model of the projectile and a rigidbody")]
+    public Rigidbody projectilePrefab;
+    [Tooltip("The force with which to fire the projectile forward")]
+    public float launchForce;
+    [Tooltip("The amount of seconds to keep the projectile in the game")]
+    public float lifetime;
+    [Tooltip("The rotation by which to offset the projectile's rotation to fire it in a different direction")]
+    public Vector3 rotationOffset;
+
+    public void Launch (Transform fireLocation)
+    {
+        Rigidbody projectile = Instantiate(projectilePrefab,
+            fireLocation.position,
+            fireLocation.rotation * Quaternion.Euler(rotationOffset)) as Rigidbody;
+
+        Quaternion rot = projectile.transform.rotation;
+        Vector3 eulerRot = rot.eulerAngles;
+        eulerRot.x = eulerRot.z = 0;
+        projectile.transform.rotation = Quaternion.Euler(eulerRot);
+
+        projectile.AddRelativeForce(Vector3.forward * launchForce);
+
+        Destroy(projectile.gameObject, lifetime);
+    }
+}
--- MightyWizards/Assets/ScriptableObjects/Staff/Staff.cs
+++ MightyWizards/Assets/ScriptableObjects/Staff/Staff.cs
@@ -4,12 +4,17 @@ using System.Collections;
 [CreateAssetMenu(menuName = "Staffs/Staff")]
 public class Staff : ScriptableObject {
 
+    [Tooltip("The model of the staff. It must have a child transform tagged as FireLocation, marking from where projectiles will fire")]
     public GameObject modelPrefab;
 
-    public GameObject basicAttack;
+    [Tooltip("The projectile this staff will fire")]
+    public Projectile basicAttack;
+    [Tooltip("The spell that goes with this staff")]
     public GameObject spell;
 
+    [Tooltip("The offset to move the staff from the spawn location")]
     public Vector3 locationOffset;
+    [Tooltip("The offset to rotate the staff around itself")]
     public Vector3 rotationOffset;
 
     private Transform fireLocation;
@@ -25,12 +30,7 @@ public class Staff : ScriptableObject {
 
     public void Attack ()
     {
-        GameObject projectile = Instantiate(basicAttack, fireLocation.position, fireLocation.rotation) as GameObject;
-        Quaternion rot = projectile.transform.rotation;
-        Vector3 eulerRot = rot.eulerAngles;
-        eulerRot.x = eulerRot.z = 0;
-        projectile.transform.rotation = Quaternion.Euler(eulerRot);
-        projectile.GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * 5000);
+        basicAttack.Launch(fireLocation);
     }
 
     public void CastSpell ()
