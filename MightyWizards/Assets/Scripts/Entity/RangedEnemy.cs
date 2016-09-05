1ec23f2a4ed2a33e2377056ce6f75d5805a5f358
123099 <khazovalex97@gmail.com>
2016-09-05 15:56:50 +0200

    enemy shooting fixed

--- MightyWizards/Assets/Scripts/Entity/RangedEnemy.cs
+++ MightyWizards/Assets/Scripts/Entity/RangedEnemy.cs
@@ -5,12 +5,18 @@ using System;
 public class RangedEnemy : Enemy {
 
     [Tooltip("The location from which the enemy shoots")]
-    private Transform fireLocation;
+    [SerializeField] private Transform fireLocation;
     [Tooltip("The projectile this enemy shoots at it's target")]
     [SerializeField] private ProjectileData projectile;
+    [Tooltip("The fire rate timer")]
+    [SerializeField] private RateTimer fireTimer;
 
     public override void Attack ()
     {
-        projectile.Launch(fireLocation);
+        if (fireTimer.IsReady())
+        {
+            Vector3 dir = target.transform.position - fireLocation.position;
+            projectile.Launch(fireLocation, Quaternion.LookRotation(dir));
+        }
     }
 }
