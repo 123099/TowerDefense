fc6ffbcf30dd6fcd2ef3eae21f3dfbf103e0077b
123099 <khazovalex97@gmail.com>
2016-09-05 15:44:31 +0200

    ranged enemies have projectiles

--- MightyWizards/Assets/Scripts/Entity/RangedEnemy.cs
+++ MightyWizards/Assets/Scripts/Entity/RangedEnemy.cs
@@ -4,8 +4,13 @@ using System;
 
 public class RangedEnemy : Enemy {
 
+    [Tooltip("The location from which the enemy shoots")]
+    private Transform fireLocation;
+    [Tooltip("The projectile this enemy shoots at it's target")]
+    [SerializeField] private ProjectileData projectile;
+
     public override void Attack ()
     {
-        print("Pew Pew");
+        projectile.Launch(fireLocation);
     }
 }
