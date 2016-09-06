54172aac6675ced085baaef826a3125da9db5e57
123099 <khazovalex97@gmail.com>
2016-09-06 20:06:08 +0200

    started difficulty setting work

---
+++ MightyWizards/Assets/DifficultySetter.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DifficultySetter : MonoBehaviour {
+
+	[Tooltip("Global value multiplier")]
+    [SerializeField] private float multiplier;
+
+    public void ApplyDifficulty ()
+    {
+        PlayerPrefs.SetFloat("Multiplier", multiplier);
+    }
+}
