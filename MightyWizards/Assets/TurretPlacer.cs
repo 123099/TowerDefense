093e5a7b29732b615a50a80d2284ffb9a035f670
123099 <khazovalex97@gmail.com>
2016-09-06 08:51:31 +0200

    started turret placer

---
+++ MightyWizards/Assets/TurretPlacer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TurretPlacer : MonoBehaviour {
+
+    [Tooltip("Turret slot button template")]
+    [SerializeField] private Button slotTemplate;
+
+    private Turret[] turretsToPlace;
+
+    public void SetTurrets(Turret[] turrets)
+    {
+        turretsToPlace = turrets;
+    }
+}
