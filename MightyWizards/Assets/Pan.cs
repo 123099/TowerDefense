72c1d25d899f27dde9a5059fe5a709ad61fd7410
123099 <khazovalex97@gmail.com>
2016-09-07 13:11:01 +0200

    added camera pan

---
+++ MightyWizards/Assets/Pan.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[DisallowMultipleComponent]
+public class Pan : MonoBehaviour {
+
+    [Tooltip("The factor by which to blend between the start and target positions each frame")]
+    [SerializeField] private float smoothing;
+
+    private Transform startPosition;
+    private Transform targetPosition;
+
+	public void PanTowards(Transform targetPosition)
+    {
+        startPosition = transform;
+        this.targetPosition = targetPosition;
+    }
+
+    private void Update ()
+    {
+        if (targetPosition)
+        {
+            transform.position = Vector3.Slerp(startPosition.position, targetPosition.position, smoothing);
+        }
+    }
+}
