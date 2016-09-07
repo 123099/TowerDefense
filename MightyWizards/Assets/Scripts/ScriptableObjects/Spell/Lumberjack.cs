1f6ab8e61afbc82082d59794ee79b959a9af638d
123099 <khazovalex97@gmail.com>
2016-09-07 14:55:38 +0200

    lumberjack updates

--- MightyWizards/Assets/Scripts/ScriptableObjects/Spell/Lumberjack.cs
+++ MightyWizards/Assets/Scripts/ScriptableObjects/Spell/Lumberjack.cs
@@ -7,13 +7,19 @@ public class Lumberjack : Spell {
 
     [Tooltip("The amount to heal the base and walls by")]
     public float healAmount;
+    [Tooltip("The amount of walls to build")]
+    [SerializeField] [Range(1,4)] private int wallsToBuild = 1;
 
     protected override void Effect ()
     {
         GameUtils.GetBase().GetComponent<Health>().Heal(healAmount);
 
-        Wall[] walls = GameUtils.GetAllWalls();
+        /**Wall[] walls = GameUtils.GetAllWalls();
         foreach (Wall wall in walls)
-            wall.GetComponent<Health>().Heal(healAmount);
+            wall.GetComponent<Health>().Heal(healAmount);*/
+
+        WizardBase wizardBase = GameUtils.GetBase();
+        for (int i = 0; i < wallsToBuild; ++i)
+            wizardBase.SpawnWall();
     }
 }
